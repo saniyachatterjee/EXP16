8de92e068071cfbeaa3be3b6f63f16b574e6e6e3
saniyachatterjee <saniyaa0103@gmail.com>
2024-09-30 12:11:10 +0530

    Create 16_1.cpp

---
+++ 16_1.cpp
@@ -0,0 +1,21 @@
+//Saniya Chatterjee
+//PRN 23070123113
+//EnTC B2
+#include <iostream>
+using namespace std;
+
+int main() {
+    int weight;
+    cout << "Enter weight: ";  
+    cin >> weight;            
+    try {
+        if (weight > 50) {    
+            throw weight;      
+        } else {
+            cout << "Weight: " << weight << "\n APPROVED" << endl;
+        }
+    }
+    catch (int a) {         
+        cout << "\n ERROR: Overweight! (" << weight << ")" << endl;  
+    }
+}
